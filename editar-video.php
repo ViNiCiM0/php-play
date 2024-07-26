a2c7ad8b0f73707e56119ca6402dd4c8de1a63b3
Mateus Ramos <pereira.mateusramos@outlook.com>
2024-07-26 11:54:25 -0300

    Criação do método alterar vídeos.

---
+++ editar-video.php
@@ -0,0 +1,32 @@
+<?php
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+if ($id === false) {
+    header("Location: index.php?sucesso=0");
+    exit();
+}
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+if ($url === false) {
+    header("Location: index.php?sucesso=0");
+    exit();
+}
+$titulo = filter_input(INPUT_POST, 'titulo', FILTER_SANITIZE_STRING);
+if ($titulo === false) {
+    header("Location: index.php?sucesso=0");
+    exit();
+}
+$sql = 'UPDATE videos SET url = :url, title = :title WHERE id = :id';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':url', $url);
+$stmt->bindValue(':title', $titulo);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+if ($stmt->execute() === false){
+    header('location: index.php?sucesso=0');
+}else{
+    header('location: index.php?sucesso=1');
+}
+
+
