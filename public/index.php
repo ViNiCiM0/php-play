77f9f8bb317fc6737db392019fa1761c8b38eb33
ViNiCiM0 <vinic.mach05@gmail.com>
2024-08-20 11:25:34 -0300

    Projeto de gerenciamento de videos. Metodos editar e excluir

--- public/index.php
+++ public/index.php
@@ -2,20 +2,22 @@
 
 //var_dump($_SERVER);
 
+require_once __DIR__ . "/../vendor/autoload.php";
+
 if (!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/'){
-    require_once __DIR__ . '/../listagem-videos.php';
+    require_once __DIR__ . '/listagem-videos.php';
 }elseif ($_SERVER['PATH_INFO'] === '/novo-video'){
     if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-        require_once __DIR__ . '/../formulario.php';
+        require_once __DIR__ . '/formulario.php';
     }elseif ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        require_once __DIR__ . '/../novo-video.php';
+        require_once __DIR__ . '/novo-video.php';
     }
 }elseif ($_SERVER['PATH_INFO'] === '/editar-video'){
     if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-        require_once __DIR__ . '/../formulario.php';
+        require_once __DIR__ . '/formulario.php';
     }elseif ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        require_once __DIR__ . '/../editar-video.php';
+        require_once __DIR__ . '/editar-video.php';
     }
 }elseif ($_SERVER['PATH_INFO'] === '/remover-video'){
-    require_once __DIR__ . '/../remover-video.php';
+    require_once __DIR__ . '/remover-video.php';
 }
